d079ed1c0f808b845621b3cbf8ac323d7d8404ec
zhaoyunxiang <zhaoyunxiang@meituan.com>
2020-01-02 18:08:47 +0800

    unlimited height sample

--- src/RouteConfig.js
+++ src/RouteConfig.js
@@ -7,11 +7,16 @@ import {
 } from "react-router-dom";
 
 import CrossTabTest2 from './crossTabTest2';
+import UnlimitedHeight from './unlimitedHeight';
 
 const routes = [
     {
       path: "/crossTabTest2",
       component: CrossTabTest2
+    },
+    {
+      path: "/unlimitedHeight",
+      component: UnlimitedHeight
     }
   ];
 
@@ -22,6 +27,9 @@ const routes = [
           <ul>
             <li>
               <Link to="/crossTabTest2">crossTabTest2</Link>
+            </li>
+            <li>
+              <Link to="/unlimitedHeight">unlimitedHeight</Link>
             </li>
           </ul>
           <Switch>
